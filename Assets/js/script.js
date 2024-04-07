b50ac1651d29833210f2f7924850234058e26fd7
SagarSuriOfficial <sagarsuriofficial@gmail.com>
2024-04-07 08:23:33 +0530

    script add on html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,16 +3,3 @@
     const body = document.body;
     body.classList.toggle('dark');
   }
-// PAGE LOADING ANIMATION 
-document.addEventListener("DOMContentLoaded", function () {
-    // Show loading animation when the page starts loading
-    document.getElementById("loading").style.display = "block";
-
-    // Hide loading animation after 2-3 seconds (adjust as needed)
-    setTimeout(function () {
-      document.getElementById("loading").style.display = "none";
-      // Show content after loading animation completes
-      document.getElementById("content").style.display = "block";
-    }, 500); // Adjust duration as needed
-  });
-
